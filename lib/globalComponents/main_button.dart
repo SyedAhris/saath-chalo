a79788c490885c780dfd1041f815e308abafd21c
ibrahimahmedkhan <ibrahimahmed.ayesha@gmail.com>
2022-11-12 20:15:12 +0500

    Created widget for commonly used button

---
+++ lib/globalComponents/main_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class mainButton extends StatelessWidget {
+  const mainButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
