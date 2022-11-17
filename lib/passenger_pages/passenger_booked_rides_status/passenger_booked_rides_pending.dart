860f2c47aa98b888f49d3342e92dcd00b39911e1
IrtizaR <irtizaraza09@gmail.com>
2022-11-17 23:48:07 +0500

    pending card complete

--- lib/passenger_pages/passenger_booked_rides_status/passenger_booked_rides_pending.dart
+++ lib/passenger_pages/passenger_booked_rides_status/passenger_booked_rides_pending.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutterdemo/globalComponents/booked_ride_card.dart';
 import 'package:flutterdemo/globalComponents/main_app_bar.dart';
+
+import '../../globalComponents/main_button.dart';
+import '../passenger_home/location_text_field.dart';
 
 class PassengerBookedRidesPending extends StatefulWidget {
   const PassengerBookedRidesPending({Key? key}) : super(key: key);
@@ -9,22 +13,60 @@ class PassengerBookedRidesPending extends StatefulWidget {
 }
 
 class _PassengerBookedRidesPendingState extends State<PassengerBookedRidesPending> {
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.transparent,
       appBar: const MainAppBar(title: "SaathChalo"),
       body: Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
             image: DecorationImage(
-                fit: BoxFit.none,
-                image: Image.asset('assets/images/mapBackground.jpg') as ImageProvider
-            )
+                fit: BoxFit.cover,
+                image:  AssetImage('assets/images/backimg.png') )
         ),
         child: SizedBox(
+          //needs to be changed so automatically fits whole screen
           height: double.infinity,
-          child: SingleChildScrollView(
-
-          ),
+          // height: 631,
+          child: Padding(
+              padding: const EdgeInsets.only(left: 50, right: 50, top: 40),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  BookedRideCard(
+                      name: "Syed Abdul Shakoor",
+                      car: "Black Suzuki WagonR",
+                      numberPlate: "ABC-123",
+                      journeyStart: "Institute of Business Administration",
+                      journeyEnd: "Askari 4",
+                      rating: 4.5,
+                      acStatus: true,
+                      journeyDate: "26/11/2022",
+                      journeyTime: "09:15",
+                      estCost: 600,
+                      status: "None"
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 20),
+                    child: LocationTextField(
+                      labelText: "PickUp",
+                      hintText: "e.g IBA - Karachi University",
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 20),
+                    child: LocationTextField(
+                        labelText: "DropOff", hintText: "e.g Chaar Meenar"),
+                  ),
+                  const Expanded(
+                    child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: MainButton(text: "Search")),
+                  )
+                ],
+              )),
         ),
       )
     );
