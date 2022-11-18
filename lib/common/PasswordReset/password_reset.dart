93eaf9c928fb857e876da4af3ab6c18e291469d6
IrtizaR <irtizaraza09@gmail.com>
2022-11-18 23:45:47 +0500

    linking

--- lib/common/PasswordReset/password_reset.dart
+++ lib/common/PasswordReset/password_reset.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutterdemo/common/EnterResetCode/enter_reset_code.dart';
+
+import '../../globalComponents/main_app_bar.dart';
 
 class PasswordReset extends StatefulWidget {
   const PasswordReset({super.key});
@@ -13,116 +15,124 @@ class PasswordReset extends StatefulWidget {
 class _PasswordResetState extends State<PasswordReset> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.fromLTRB(20, 80, 0, 20),
-                child: Icon(
-                  Icons.lock_person,
-                  size: 100.0,
-                  color: Color(0xffF65E5D),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
-                child: Text(
-                  'Reset Password',
-                  style: TextStyle(
-                      fontSize: 32,
-                      fontFamily: 'Nunito',
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Flexible(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Text(
-                    "Please provide with your account email address that you previously used to login with.",
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 3,
-                    style: TextStyle(fontSize: 16, fontFamily: 'Nunito'),
+    return Scaffold(
+      appBar: const MainAppBar(title: "SaathChalo"),
+      body: Container(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(20, 80, 0, 20),
+                  child: Icon(
+                    Icons.lock_person,
+                    size: 100.0,
+                    color: Color(0xffF65E5D),
                   ),
                 ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Flexible(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
                   child: Text(
-                    "A code will be sent to this email.",
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
+                    'Reset Password',
                     style: TextStyle(
-                        fontSize: 16,
+                        fontSize: 32,
                         fontFamily: 'Nunito',
                         fontWeight: FontWeight.bold),
                   ),
                 ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: const BorderSide(
-                    color: Color(0xffDCDEDF),
-                  ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: const BorderSide(
-                    color: Color(0xffDCDEDF),
-                  ),
-                ),
-                labelText: 'E-mail',
-                hintText: 'example@gmail.com',
-                errorText:
-                    'Oops! The following email address is not linked with any account',
-                errorMaxLines: 2,
-              ),
+              ],
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.0),
-            child: SizedBox(
-              width: 355,
-              height: 50,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Flexible(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Text(
+                      "Please provide with your account email address that you previously used to login with.",
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 3,
+                      style: TextStyle(fontSize: 16, fontFamily: 'Nunito'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Flexible(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Text(
+                      "A code will be sent to this email.",
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: 'Nunito',
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(8),
+                    borderSide: const BorderSide(
+                      color: Color(0xffDCDEDF),
+                    ),
                   ),
-                ),
-                onPressed: () {},
-                child: const Text(
-                  "Send Reset Code",
-                  style: TextStyle(fontSize: 18),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: const BorderSide(
+                      color: Color(0xffDCDEDF),
+                    ),
+                  ),
+                  labelText: 'E-mail',
+                  hintText: 'example@gmail.com',
+                  errorText:
+                      'Oops! The following email address is not linked with any account',
+                  errorMaxLines: 2,
                 ),
               ),
             ),
-          ),
-        ],
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: SizedBox(
+                width: 355,
+                height: 50,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(builder:
+                    (context)=> EnterResetCode()
+                    )
+                    );
+                  },
+                  child: const Text(
+                    "Send Reset Code",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
