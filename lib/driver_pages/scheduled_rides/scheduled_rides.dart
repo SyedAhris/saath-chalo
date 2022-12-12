b643bf30fd2a14b49211242f631f3d6b2de320fa
Hamza <hamza.salmann9@gmail.com>
2022-12-12 18:38:35 +0500

    addition of history screens

--- lib/driver_pages/scheduled_rides/scheduled_rides.dart
+++ lib/driver_pages/scheduled_rides/scheduled_rides.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutterdemo/globalComponents/card_list.dart';
 import 'package:flutterdemo/globalComponents/ride_history_card.dart';
 
 class ScheduledRides extends StatelessWidget {
