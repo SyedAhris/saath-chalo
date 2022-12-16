c5a5ed771e1128c45a1f8f6da8ac0fe86e7159ed
SyedAhris <ahrissyed@gmail.com>
2022-12-16 20:42:42 +0500

    scheduled_rides.dart link with mock completed

--- lib/driver_pages/scheduled_rides/scheduled_rides.dart
+++ lib/driver_pages/scheduled_rides/scheduled_rides.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutterdemo/constants/convert_time.dart';
 import 'package:flutterdemo/driver_pages/scheduled_ride_booking/scheduled_ride_booking.dart';
 import 'package:flutterdemo/driver_pages/scheduled_ride_details/scheduled_ride_details.dart';
 import 'package:flutterdemo/global_components/driver_side_bar.dart';
 import 'package:flutterdemo/global_components/main_app_bar.dart';
 import 'package:flutterdemo/passenger_pages/passenger_booked_rides/booked_rides_demo.dart';
+import 'package:flutterdemo/providers_repositories/current_user/current_user_provider.dart';
+import 'package:flutterdemo/providers_repositories/driver/scheduled_rides/driver_scheduled_rides_provider.dart';
+import 'package:provider/provider.dart';
 
 import '../../global_components/ride_card.dart';
 
@@ -16,7 +20,18 @@ class ScheduledRides extends StatefulWidget {
 
 class _ScheduledRidesState extends State<ScheduledRides> {
   @override
+  void initState() {
+    super.initState();
+    context
+        .read<DriverScheduledRidesProvider>()
+        .fetchRides(context.read<CurrentUserProvider>().currentCustomer.id);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final rideNow = context.watch<DriverScheduledRidesProvider>().rideNow;
+    final upcomingRides =
+        context.watch<DriverScheduledRidesProvider>().upcomingRide;
     return Scaffold(
       drawer: DriverSideBar(),
       appBar: const MainAppBar(title: "Scheduled Rides"),
@@ -31,10 +46,8 @@ class _ScheduledRidesState extends State<ScheduledRides> {
                     padding: EdgeInsets.only(bottom: 10),
                     child: Text(
                       "Ride Now",
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold
-                      ),
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                     ),
                   ),
                   Expanded(
@@ -46,42 +59,40 @@ class _ScheduledRidesState extends State<ScheduledRides> {
                   ),
                 ],
               ),
-              ListView.builder(
-                  primary: false,
-                  shrinkWrap: true,
-                  itemCount: rideNow.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Column(
-                      children: [
-                        InkWell(
-                          onTap:(){
-                            Navigator.of(context).push(
-                                MaterialPageRoute(
-                                    builder: (context) => const ScheduledRideBooking()
-                                )
-                            );
-                          },
-                          child: RideCard(
-                            journeyStart: rideNow[index].journeyStart,
-                            journeyEnd: rideNow[index].journeyEnd,
-                            journeyDate: rideNow[index].journeyDate,
-                            journeyTime: rideNow[index].journeyTime,
-                            estCost: rideNow[index].estCost,
-                          ),
-                        ),
-                      ],
-                    );
-                  }),
+              context.watch<DriverScheduledRidesProvider>().isFetching
+                  ? const CircularProgressIndicator()
+                  : ListView.builder(
+                      primary: false,
+                      shrinkWrap: true,
+                      itemCount: rideNow.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Column(
+                          children: [
+                            InkWell(
+                              onTap: () {
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) =>
+                                        const ScheduledRideBooking()));
+                              },
+                              child: RideCard(
+                                journeyStart: rideNow[index].startingDestination,
+                                journeyEnd: rideNow[index].endingDestination,
+                                journeyDate: ConvertTime.millisecondsToDate(rideNow[index].date),
+                                journeyTime: ConvertTime.millisecondsToTime(rideNow[index].time),
+                                estCost: rideNow[index].totalFare.toDouble(),
+                              ),
+                            ),
+                          ],
+                        );
+                      }),
               Row(
                 children: const [
                   Padding(
-                    padding: EdgeInsets.only(top:10,bottom: 10),
+                    padding: EdgeInsets.only(top: 10, bottom: 10),
                     child: Text(
                       "Upcoming Rides",
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold
-                      ),
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                     ),
                   ),
                   Expanded(
@@ -101,19 +112,17 @@ class _ScheduledRidesState extends State<ScheduledRides> {
                     return Column(
                       children: [
                         InkWell(
-                          onTap:(){
-                            Navigator.of(context).push(
-                                MaterialPageRoute(
-                                    builder: (context) => const ScheduledRideDetails()
-                                )
-                            );
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) =>
+                                    const ScheduledRideDetails()));
                           },
                           child: RideCard(
-                            journeyStart: upcomingRides[index].journeyStart,
-                            journeyEnd: upcomingRides[index].journeyEnd,
-                            journeyDate: upcomingRides[index].journeyDate,
-                            journeyTime: upcomingRides[index].journeyTime,
-                            estCost: upcomingRides[index].estCost,
+                            journeyStart: upcomingRides[index].startingDestination,
+                            journeyEnd: upcomingRides[index].endingDestination,
+                            journeyDate: ConvertTime.millisecondsToTime(upcomingRides[index].date),
+                            journeyTime: ConvertTime.millisecondsToTime(upcomingRides[index].time),
+                            estCost: upcomingRides[index].totalFare.toDouble(),
                           ),
                         ),
                       ],
@@ -127,35 +136,31 @@ class _ScheduledRidesState extends State<ScheduledRides> {
   }
 }
 
-List<BookedRidesDemo> rideNow = [
-  BookedRidesDemo(
-      journeyStart: "Institute of Business Administration",
-      journeyEnd: "Askari 4",
-      journeyDate: "26/11/2022",
-      journeyTime: "09:15",
-      estCost: 600
-  ),
-  BookedRidesDemo(
-      journeyStart: "Institute of Business Administration",
-      journeyEnd: "Askari 4",
-      journeyDate: "26/11/2022",
-      journeyTime: "09:15",
-      estCost: 600
-  ),
-];
-List<BookedRidesDemo> upcomingRides = [
-  BookedRidesDemo(
-      journeyStart: "Institute of Business Administration",
-      journeyEnd: "Askari 4",
-      journeyDate: "26/11/2022",
-      journeyTime: "09:15",
-      estCost: 600
-  ),
-  BookedRidesDemo(
-      journeyStart: "Institute of Business Administration",
-      journeyEnd: "Askari 4",
-      journeyDate: "26/11/2022",
-      journeyTime: "09:15",
-      estCost: 600
-  ),
-];
+// List<BookedRidesDemo> rideNow = [
+//   BookedRidesDemo(
+//       journeyStart: "Institute of Business Administration",
+//       journeyEnd: "Askari 4",
+//       journeyDate: "26/11/2022",
+//       journeyTime: "09:15",
+//       estCost: 600),
+//   BookedRidesDemo(
+//       journeyStart: "Institute of Business Administration",
+//       journeyEnd: "Askari 4",
+//       journeyDate: "26/11/2022",
+//       journeyTime: "09:15",
+//       estCost: 600),
+// ];
+// List<BookedRidesDemo> upcomingRides = [
+//   BookedRidesDemo(
+//       journeyStart: "Institute of Business Administration",
+//       journeyEnd: "Askari 4",
+//       journeyDate: "26/11/2022",
+//       journeyTime: "09:15",
+//       estCost: 600),
+//   BookedRidesDemo(
+//       journeyStart: "Institute of Business Administration",
+//       journeyEnd: "Askari 4",
+//       journeyDate: "26/11/2022",
+//       journeyTime: "09:15",
+//       estCost: 600),
+// ];
