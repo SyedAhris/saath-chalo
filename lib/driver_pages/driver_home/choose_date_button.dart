49aed5679bd248609a59dcad2df299f198456128
IrtizaR <irtizaraza09@gmail.com>
2022-12-16 22:00:29 +0500

    final driver screens i think

--- lib/driver_pages/driver_home/choose_date_button.dart
+++ lib/driver_pages/driver_home/choose_date_button.dart
@@ -1,23 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:flutterdemo/constants/constants.dart';
 
-class ChooseDateButton extends StatelessWidget {
+class ChooseDateButton extends StatefulWidget {
   const ChooseDateButton({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<ChooseDateButton> createState() => _ChooseDateButtonState();
+}
+
+class _ChooseDateButtonState extends State<ChooseDateButton> {
+  late int dateDay;
+  late int dateMonth;
+  late int dateYear;
+  late DateTime dateChosen;
+  bool dateSelected=false;
+
+  @override
   Widget build(BuildContext context) {
     return TextButton(
-        style: TextButton.styleFrom(
-            primary: Colors.black, backgroundColor: Colors.white),
-        onPressed: () => {},
+        style: dateSelected==false?
+        TextButton.styleFrom(
+            foregroundColor: Colors.black, backgroundColor: Colors.white)
+        :
+        TextButton.styleFrom(
+            foregroundColor: Colors.white, backgroundColor: MainColors.primary)
+        ,
+        onPressed: () {
+          DatePicker.showDatePicker(context,
+              showTitleActions: true,
+              minTime: DateTime(DateTime.now().year,DateTime.now().month,DateTime.now().day),
+              maxTime: DateTime(DateTime.now().year+5, 12, 31), onChanged: (date) {
+              },
+              onConfirm: (date) {
+                dateDay=date.day;
+                dateMonth=date.month;
+                dateYear=date.year;
+                dateSelected=true;
+                dateChosen=date;
+                setState(() {
+                });
+              },
+              currentTime: DateTime.now(), locale: LocaleType.en);
+        },
         child: Row(
-          children: const [
-            Icon(
+          children: [
+            const Icon(
               Icons.calendar_today_outlined,
               size: 20,
             ),
-            Text("Choose Date"),
+            Container(
+              child: dateSelected==false?
+              const Text("Choose Date")
+              :
+              Text(" $dateDay/$dateMonth/$dateYear"),
+            )
           ],
         ));
   }
--- lib/driver_pages/driver_home/chose_time_button.dart
+++ lib/driver_pages/driver_home/chose_time_button.dart
@@ -1,23 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:flutterdemo/constants/constants.dart';
 
-class ChoseTimeButton extends StatelessWidget {
+class ChoseTimeButton extends StatefulWidget {
   const ChoseTimeButton({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<ChoseTimeButton> createState() => _ChoseTimeButtonState();
+}
+
+class _ChoseTimeButtonState extends State<ChoseTimeButton> {
+  late int timeHour;
+  late int timeMinute;
+  late DateTime timeChosen;
+  bool timeSelected=false;
+
+  @override
   Widget build(BuildContext context) {
     return TextButton(
-        style: TextButton.styleFrom(
-            primary: Colors.black, backgroundColor: Colors.white),
-        onPressed: () => {},
+        style: timeSelected==false?
+        TextButton.styleFrom(
+            foregroundColor: Colors.black, backgroundColor: Colors.white)
+            :
+        TextButton.styleFrom(
+            foregroundColor: Colors.white, backgroundColor: MainColors.primary)
+        ,
+        onPressed: () {
+          DatePicker.showTime12hPicker(context, showTitleActions: true,
+              onChanged: (date) {
+              },
+              onConfirm: (date) {
+                timeHour=date.hour;
+                timeMinute=date.minute;
+                timeSelected=true;
+                timeChosen=date;
+                setState(() {
+                });
+              },
+              currentTime: DateTime.now());
+        },
         child: Row(
-          children: const [
-            Icon(
+          children:  [
+            const Icon(
               Icons.access_time_outlined,
               size: 20,
             ),
-            Text("Choose Time"),
+            Container(
+              child: timeSelected==false?
+              const Text("Choose Date")
+                  :
+              Text(" $timeHour:$timeMinute"),
+            )
           ],
         ));
   }
