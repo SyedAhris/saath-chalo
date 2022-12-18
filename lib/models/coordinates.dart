5c5693bd195899d1b7ac2d2d7e5b7bf2d682d80b
ibrahimahmedkhan <ibrahimahmed.ayesha@gmail.com>
2022-12-18 20:03:46 +0500

    Added LatLng converter to coordiinates class

--- lib/models/coordinates.dart
+++ lib/models/coordinates.dart
@@ -1,3 +1,5 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 class Coordinates {
   const Coordinates({
     required this.lat,
@@ -16,4 +18,6 @@ class Coordinates {
         "lat": lat,
         "long": long,
       };
+
+  LatLng toLatLng() => LatLng(double.parse(lat), double.parse(long));
 }
